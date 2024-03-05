1151b4dd11f3ef752db089e2a7024d8eee969f07
mahonch <maxim.p811@mail.ru>
2024-03-05 19:53:56 +0300

    Контейнер Column

--- app/src/main/java/com/raywenderlich/android/lab1/screens/ColumnScreen.kt
+++ app/src/main/java/com/raywenderlich/android/lab1/screens/ColumnScreen.kt
@@ -1,21 +1,40 @@
 package com.raywenderlich.android.lab1.screens
 
+import androidx.compose.foundation.layout.Arrangement
+import androidx.compose.foundation.layout.Column
+import androidx.compose.foundation.layout.fillMaxSize
+import androidx.compose.material.Text
 import androidx.compose.runtime.Composable
-import androidx.compose.ui.graphics.BlendMode.Companion.Screen
-import com.raywenderlich.android.lab1.app.FundamentalsApp
+import androidx.compose.ui.Alignment
+import androidx.compose.ui.Modifier
+import androidx.compose.ui.res.stringResource
+import androidx.compose.ui.unit.sp
 import com.raywenderlich.android.lab1.router.BackButtonHandler
 import com.raywenderlich.android.lab1.router.FundamentalsRouter
+import com.raywenderlich.android.lab1.router.Screen
 
 @Composable
-fun ColumnScreen (){
+fun ColumnScreen()
+{
     MyColumn()
 
     BackButtonHandler {
-        FundamentalsRouter.navigateTo(com.raywenderlich.android.lab1.router.Screen.Navigation)
+        FundamentalsRouter.navigateTo(Screen.Navigation)
     }
-}
+}@Composable
+fun MyColumn()
+{
+    Column(
 
-@Composable
-fun MyColumn() {
-    TODO("Not yet implemented")
+        horizontalAlignment =Alignment.CenterHorizontally,
+        verticalArrangement =Arrangement.SpaceEvenly,
+        modifier =Modifier.fillMaxSize()
+    ){
+        THREE_ELEMENT_LIST.forEach{ textResId ->
+            Text(
+                text = stringResource(id = textResId),
+                fontSize =22.sp
+            )
+        }
+    }
 }
